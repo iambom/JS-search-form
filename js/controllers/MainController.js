6b0129b144d890225476bcf5dcc94a3208c0975c
보미니 <iambxmk@gmail.com>
2021-04-27 22:32:10 +0900

    TabView and KeywordView

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,7 +1,10 @@
 import FormView from "../views/FormView.js";
 import ResultView from "../views/ResultView.js";
+import TabView from "../views/TabView.js";
+import KeywordView from "../views/KeywordView.js";
 
 import SearchModel from "../models/SearchModel.js";
+import KeywordModel from "../models/KeywordModel.js";
 
 const tag = '[MainController]';
 
@@ -11,11 +14,39 @@ export default {
     .on('@submit', e => this.onSubmit(e.detail.input))
       .on("@reset", e => this.onResetForm());
 
+    TabView.setup(document.querySelector('#tabs'))
+      .on('@change', e => this.onChangeTab(e.detail.tabName));
+
+    KeywordView.setup(document.querySelector('#search-keyword'))
+      .on('@click', e => this.onClickKeyword(e.detail.keyword));
+
     ResultView.setup(document.querySelector('#search-result'));
+
+    this.selectedTab = '추천 검색어';
+    this.renderView();
+  },
+
+  renderView(){
+    TabView.setActiveTab(this.selectedTab);
+
+    if (this.selectedTab === '추천 검색어') {
+      console.log(tag, 'renderView()')
+      this.fetchSearchKeyword();
+    }else {
+
+    }
+
+    ResultView.hide();
+  },
+
+  fetchSearchKeyword() {
+    KeywordModel.list().then(data => {
+      KeywordView.render(data);
+    })
   },
 
   search(query) {
-    console.log(tag, 'search()', query);
+    FormView.setValue(query);
     SearchModel.list(query).then(data => {
       this.onSearchResult(data);
     });
@@ -28,10 +59,20 @@ export default {
 
   onResetForm() {
     console.log(tag, 'onResetForm()');
-    ResultView.hide();
+    this.renderView();
   },
 
   onSearchResult(data) {
+    TabView.hide();
+    KeywordView.hide();
     ResultView.render(data);
+  },
+
+  onChangeTab(tabName) {
+    // debugger
+  },
+
+  onClickKeyword(keyword) {
+    this.search(keyword)
   },
 }
